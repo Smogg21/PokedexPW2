b5ab7f9293fe6f72fe02e8a1d90fa0ed1ce6e374
David Ildefonso <david_lion_ig@outlook.com.pe>
2024-07-16 18:02:09 -0500

    cambiar por imagenes

--- src/app/poke-run/poke-run.component.ts
+++ src/app/poke-run/poke-run.component.ts
@@ -12,38 +12,37 @@ export class PokeRunComponent implements OnInit {
   private ctx!: CanvasRenderingContext2D;
   private player!: { x: number, y: number, width: number, height: number, dy: number };
   private gravity: number;
-  private obstacles: { x: number, y: number, width: number, height: number, speed: number }[];
-  private obstacleSpawnInterval: number;
+  private obstacles: { x: number, y: number, width: number, height: number, speed: number }[];;
+  private obstacleInterval: number;
   private isJumping: boolean;
-  public isGameOver: boolean;
-  public isGameStarted: boolean;
-  public score: number;
-  private startTime!: number;
-  public elapsedTime: number;
-  private obstacleSpeed: number;
-  private initialObstacleSpeed: number;
-  private obstacleAcceleration: number;
-  private minObstacleGap: number;
-  private maxObstacleGap: number;
+   isGameOver: boolean;
+   isGameStarted: boolean;
+   score: number;
+   startTime!: number;
+   elapsedTime: number;
+   private obstacleSpeed: number;
+   hasWon: boolean;
+   private playerImage: HTMLImageElement = new Image();
+  private obstacleImage: HTMLImageElement = new Image();
 
   constructor() {
     this.gravity = 0.5;
     this.obstacles = [];
-    this.obstacleSpawnInterval = 0;
+    this.obstacleInterval = 0;
     this.isJumping = false;
-    this.isGameOver = false;
-    this.isGameStarted = false;
-    this.player = { x: 50, y: 0, width: 50, height: 50, dy: 0 };
-    this.score = 0;
-    this.elapsedTime = 0;
-    this.obstacleSpeed = 2;
-    this.initialObstacleSpeed = 2;
-    this.obstacleAcceleration = 0.1;
-    this.minObstacleGap = 80;
-    this.maxObstacleGap = 100;
+	this.isGameOver = false;
+	this.isGameStarted = false;
+	this.player = { x: 50, y: 0, width: 50, height: 50, dy: 0 };
+	this.score = 0;
+	this.elapsedTime = 0;
+	this.obstacleSpeed = 2;
+	 this.hasWon = false;
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+	this.playerImage.src = '../../assets/images/filter.png';
+    this.obstacleImage.src = '../../assets/images/filter.png';
+  }
 
   @HostListener('window:keydown', ['$event'])
   handleKeyDown(event: KeyboardEvent) {
@@ -57,22 +56,24 @@ export class PokeRunComponent implements OnInit {
     this.isGameStarted = true;
     this.isGameOver = false;
     this.obstacles = [];
-    this.obstacleSpawnInterval = 0;
-    this.score = 0;
-    this.elapsedTime = 0;
-    this.startTime = Date.now();
-    this.obstacleSpeed = this.initialObstacleSpeed;
+    this.obstacleInterval = 0;
+	this.score = 0;
+	this.elapsedTime = 0;
+    this.startTime = Date.now(); 
+	this.obstacleSpeed = 2;
+	 this.hasWon = false;
 
-    setTimeout(() => {
-      this.canvas = document.getElementById('gameCanvas') as HTMLCanvasElement;
-      this.ctx = this.canvas.getContext('2d')!;
-      this.player = { x: 50, y: this.canvas.height - 50, width: 50, height: 50, dy: 0 };
-      this.gameLoop();
-    }, 0);
+	setTimeout(() => {
+		this.canvas = document.getElementById('gameCanvas') as HTMLCanvasElement;
+		this.ctx = this.canvas.getContext('2d')!;
+		this.player = { x: 50, y: this.canvas.height - 50, width: 50, height: 50, dy: 0 };
+		this.gameLoop();
+	}, 0);
+	
   }
 
   gameLoop() {
-    if (this.isGameOver) {
+    if (this.isGameOver || this.hasWon) {
       this.showGameOver();
       return;
     }
@@ -91,37 +92,40 @@ export class PokeRunComponent implements OnInit {
       this.isJumping = false;
     }
 
-    this.obstacleSpawnInterval++;
-    if (this.obstacleSpawnInterval > this.calculateObstacleInterval()) {
-      let obstacleSpeed = this.initialObstacleSpeed + this.obstacleAcceleration * this.elapsedTime;
+    this.obstacleInterval++;
+    if (this.obstacleInterval > 100) {
+	  let obstacleSpeed = this.obstacleSpeed + this.elapsedTime * 0.05; 
+	
       this.obstacles.push({ x: this.canvas.width, y: this.canvas.height - 50, width: 20, height: 50, speed: obstacleSpeed });
-      this.obstacleSpawnInterval = 0;
+      this.obstacleInterval = 0;
     }
 
     this.obstacles.forEach(obstacle => {
       obstacle.x -= obstacle.speed;
-      if (this.checkCollision(this.player, obstacle)) {
+	  if (this.checkCollision(this.player, obstacle)) {
         this.isGameOver = true;
       }
     });
 
-    this.obstacles = this.obstacles.filter(obstacle => obstacle.x + obstacle.width > 0);
+    this.obstacles = this.obstacles.filter(obstacle => {
+      if (obstacle.x + obstacle.width > 0) {
+        return true;
+      } else {
+        this.score++;
+        return false;
+      }
+    });
 
-    this.elapsedTime = Math.floor((Date.now() - this.startTime) / 1000);
+	this.elapsedTime = Math.floor((Date.now() - this.startTime) / 1000);
+
+	if (this.score >= 10) {
+      this.isGameOver = true;
+      this.hasWon = true;
+    }
   }
 
-  calculateObstacleInterval(): number {
-    let minGap = this.minObstacleGap;
-    let maxGap = this.maxObstacleGap;
-    let progress = this.elapsedTime / 10; // Ajustar este valor según la velocidad deseada de incremento
-    let gap = maxGap - (maxGap - minGap) * Math.exp(-progress);
-    return Math.floor(gap);
-  }
-
-  checkCollision(
-    player: { x: number; y: number; width: number; height: number },
-    obstacle: { x: number; y: number; width: number; height: number }
-  ): boolean {
+   checkCollision(player: { x: number, y: number, width: number, height: number }, 
+                 obstacle: { x: number, y: number, width: number, height: number }): boolean {
     return (
       player.x < obstacle.x + obstacle.width &&
       player.x + player.width > obstacle.x &&
@@ -133,27 +137,25 @@ export class PokeRunComponent implements OnInit {
   draw() {
     this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
-    this.ctx.fillStyle = 'blue';
-    this.ctx.fillRect(this.player.x, this.player.y, this.player.width, this.player.height);
+    this.ctx.drawImage(this.playerImage, this.player.x, this.player.y, this.player.width, this.player.height);
 
-    this.ctx.fillStyle = 'red';
-    this.obstacles.forEach(obstacle => {
-      this.ctx.fillRect(obstacle.x, obstacle.y, obstacle.width, obstacle.height);
-    });
+	this.obstacles.forEach(obstacle => {
+		this.ctx.drawImage(this.obstacleImage, obstacle.x, obstacle.y, obstacle.width, obstacle.height);
+	});
 
-    this.ctx.fillStyle = 'black';
+	this.ctx.fillStyle = 'black';
     this.ctx.font = '20px Arial';
     this.ctx.fillText(`Score: ${this.score}`, 10, 20);
 
-    this.ctx.fillText(`Time: ${this.elapsedTime}s`, this.canvas.width - 100, 20);
+	this.ctx.fillText(`Tiempo: ${this.elapsedTime}s`, this.canvas.width - 100, 20);
   }
 
-  showGameOver() {
+   showGameOver() {
     this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
     this.ctx.fillStyle = 'black';
     this.ctx.font = '30px Arial';
     this.ctx.textAlign = 'center';
     this.ctx.fillText('Game Over', this.canvas.width / 2, this.canvas.height / 2);
-    this.ctx.fillText(`Score: ${this.score}`, this.canvas.width / 2, this.canvas.height / 2 + 40);
+	this.ctx.fillText(`Score: ${this.score}`, this.canvas.width / 2, this.canvas.height / 2 + 40);
   }
 }
